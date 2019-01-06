c1a56f851a21e022c3ee92409da9254d22745c99
Jartreg <Jartreg@users.noreply.github.com>
2019-01-06 17:55:05 +0100

    Fix installation command

--- stage1/04-t-ag-apt-repository/00-run.sh
+++ stage1/04-t-ag-apt-repository/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 curl -s "https://technik-ag.github.io/apt-repo/repo.deb" --output "${ROOTFS_DIR}/t-ag-apt-repository.deb"
-on_chroot apt-get install "/t-ag-apt-repository.deb"
+on_chroot -c "apt-get install /t-ag-apt-repository.deb"
 rm "${ROOTFS_DIR}/t-ag-apt-repository.deb"
