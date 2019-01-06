37e6285a32d6dcbd403f6831ec5e0f9c7538aabc
Jartreg <Jartreg@users.noreply.github.com>
2019-01-06 21:14:07 +0100

    Update APT after adding the repository

--- stage1/04-t-ag-apt-repository/00-run.sh
+++ stage1/04-t-ag-apt-repository/00-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
 curl -s "https://technik-ag.github.io/apt-repo/repo.deb" --output "${ROOTFS_DIR}/t-ag-apt-repository.deb"
-on_chroot -c "apt-get install /t-ag-apt-repository.deb"
+on_chroot << EOF
+apt-get install /t-ag-apt-repository.deb
+apt-get update
+EOF
 rm "${ROOTFS_DIR}/t-ag-apt-repository.deb"
