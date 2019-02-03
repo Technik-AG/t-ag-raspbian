cbea6603b791a3235fa2361dea6ebd0c026e3f79
Jartreg <jartreg@users.noreply.github.com>
2019-02-03 18:44:07 +0100

    Fix missing apt confirmation

--- stage1/04-t-ag-packages/00-run.sh
+++ stage1/04-t-ag-packages/00-run.sh
@@ -2,7 +2,7 @@
 
 curl -s "https://technik-ag.github.io/apt-repo/repo.deb" --output "${ROOTFS_DIR}/t-ag-apt-repository.deb"
 on_chroot << EOF
-apt-get install /t-ag-apt-repository.deb
+apt-get install -y /t-ag-apt-repository.deb
 apt-get update
 EOF
 rm "${ROOTFS_DIR}/t-ag-apt-repository.deb"
